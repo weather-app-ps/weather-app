f6aa81e97285847bdcf276401d1d0162d2ebe4d3
Hallie Liu <hallieliu@umass.edu>
2023-06-27 16:42:33 -0400

    working on grabbing temp, changed grabCoord to searchCurrTemp

--- main-page/main-page.js
+++ main-page/main-page.js
@@ -1,19 +1,22 @@
-const global = {
+var global = {
     api: {
         api_key: "2900a9d94e25de2983610e7e1aed3e21",
-        // api_url: `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},US&appid=2900a9d94e25de2983610e7e1aed3e21`
+        // api_url_loc: `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},US&appid=2900a9d94e25de2983610e7e1aed3e21`
     }
 }
 
 //var searchbtn = document.querySelector(#search);
 
-async function grabCoord(query) {
+async function searchCurrTemp(query) {
     //let result = `http://api.openweathermap.org/geo/1.0/zip?zip=${query},US&appid=2900a9d94e25de2983610e7e1aed3e21`;
-    let result = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${query},US&appid=2900a9d94e25de2983610e7e1aed3e21`);
-    let data = await result.json();
+    let location = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${query},US&appid=2900a9d94e25de2983610e7e1aed3e21`);
+    let data = await location.json();
     let lat = data.lat;
     let lon = data.lon;
-    console.log(lat,lon);
+    
+    let tempGrab = await fetch (`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=2900a9d94e25de2983610e7e1aed3e21`);
+    let result = await tempGrab.json();
+    console.log(result);
 }
 
   
@@ -24,7 +27,7 @@ function init() {
     //     //returns the info
     // });
 
-    grabCoord("02478");
+    searchCurrTemp("02478");
 }
 
 window.addEventListener('DOMContentLoaded', init);

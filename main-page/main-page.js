e6dc997875fa9901413ca76ffe0ab69efb46fbac
Hallie Liu <halliu@publicisgroupe.net>
2023-06-27 14:05:22 -0400

    started work on inputting zipcode into api url to search

--- main-page/main-page.js
+++ main-page/main-page.js
@@ -1,10 +1,22 @@
 const global = {
     api: {
         api_key: "2900a9d94e25de2983610e7e1aed3e21",
-        api_url: `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=2900a9d94e25de2983610e7e1aed3e21`;
+        api_url: `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},US&appid=2900a9d94e25de2983610e7e1aed3e21`
     }
 }
 
-async function displayCurrTemp() {
+//var searchbtn = document.querySelector(#search);
+
+async function displayCurrTemp(query) {
     const {currTemp} = await fetchAPIData();
 }
+
+function init() {
+    form.addEventListener('submit', (zipcode) => {
+        //inputs user input zipcode into api url
+        //returns the info
+    });
+
+}
+
+window.addEventListener('DOMContentLoaded', init);

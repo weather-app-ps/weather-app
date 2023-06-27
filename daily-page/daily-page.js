9bf8cd8a8bb2ec1c8069c778e74a5ce7a136990b
buzzhamtaro@gmail.com <buzzhang@publicisgroupe.net>
2023-06-27 15:56:22 -0400

    day to day prints

--- daily-page/daily-page.js
+++ daily-page/daily-page.js
@@ -1,12 +1,39 @@
-import global from "../main-page/main-page.js";
+// import global from "../main-page/main-page.js";
 
-async function dayByDay(zip_code)
+const global = {
+    api: {
+        api_key: "2900a9d94e25de2983610e7e1aed3e21",
+        api_url: `https://api.openweathermap.org/data/3.0/`,
+    }
+};
+
+async function getCords(zip_code)
 {
-    const cords = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${zip_code},US&appid=${global.api_key}`);
+    const request = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${zip_code},US&appid=${global.api.api_key}`);
+    const cords = await request.json();
     console.log(cords);
-    //const response = await fetch("")
-    //hiii this is a test
-    console.log("zxxxx");
+    return cords;
 }
 
-dayByDay("02120");
+async function getWeather(zip_code)
+{
+    const cords = await getCords(zip_code);
+    const request = await fetch(`https://api.weather.gov/points/${cords.lat},${cords.lon}`);
+    const weather = await request.json();
+
+    const request_forecast = await fetch(weather.properties.forecast);
+    const forecast = (await request_forecast.json()).properties;
+    console.log(forecast);
+
+    let date = new Date();
+    for (let i = 0; i < 5; i++)
+    {
+        date.setDate(date.getDate() + 1);
+        console.log (date.getMonth() + 1, date.getDate());
+        console.log(`High ${forecast.periods[i * 2].temperature}, Low ${forecast.periods[i * 2 + 1].temperature}`);
+        console.log(`Precipitation is ${forecast.periods[i*2].probabilityOfPrecipitation.value}`)
+    }
+}
+console.log(global);
+
+getWeather("02120");
--- main-page/main-page.js
+++ main-page/main-page.js
@@ -1,10 +1,13 @@
 const global = {
     api: {
         api_key: "2900a9d94e25de2983610e7e1aed3e21",
-        api_url: `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=2900a9d94e25de2983610e7e1aed3e21`;
+        api_url: `https://api.openweathermap.org/data/3.0/`,
     }
-}
+};
 
 async function displayCurrTemp() {
     const {currTemp} = await fetchAPIData();
 }
+
+console.log(global);
+export default global;
